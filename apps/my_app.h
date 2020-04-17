ce574d1de4b85c35f01eeb63609934c7264e2c46
asochowski <39016291+asochowski@users.noreply.github.com>
2020-04-17 17:32:44 -0500

    Added b2World and basic body rendering with OpenGL

--- apps/my_app.h
+++ apps/my_app.h
@@ -3,18 +3,28 @@
 #ifndef FINALPROJECT_APPS_MYAPP_H_
 #define FINALPROJECT_APPS_MYAPP_H_
 
-#include <cinder/app/App.h>
 
+#include <cinder/app/App.h>
+#include <Box2D/Box2D.h>
 
 namespace myapp {
 
 class MyApp : public cinder::app::App {
- public:
+public:
   MyApp();
   void setup() override;
   void update() override;
   void draw() override;
   void keyDown(cinder::app::KeyEvent) override;
+  void keyUp(cinder::app::KeyEvent) override;
+
+  void DrawBodies();
+  void CreateRect(double x, double y, double h, double w, double angle);
+  void CreateBoundaries();
+  b2Body* CreatePlayer(double x, double y, double h, double w, double angle);
+private:
+  b2World* game_world;
+  b2Body* player;
 };
 
 }  // namespace myapp
---
+++ include/player.h
@@ -0,0 +1,14 @@
+//
+// Created by Aidan on 4/17/2020.
+//
+
+#ifndef FINALPROJECT_PLAYER_H
+#define FINALPROJECT_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //FINALPROJECT_PLAYER_H
