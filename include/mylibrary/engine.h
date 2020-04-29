b35e3d97b75fd307c9ad2141abb6e4ce1e59c93d
asochowski <39016291+asochowski@users.noreply.github.com>
2020-04-29 14:46:08 -0500

    Added comments to engine.h

--- include/mylibrary/engine.h
+++ include/mylibrary/engine.h
@@ -66,63 +66,191 @@ public:
   // Creates the game engine given the current screen width and height, in
   // pixels.
   Engine(int screen_width, int screen_height);
+
+  // Empty constructor of the engine.
   Engine();
+
+  // Moves the physics engine by one "tick" (about 1/60 of a second). All
+  // objects are moved to their updated positions in the b2World.
   void Step();
+
+  // Registers that a key is being pressed down by the player, given the current
+  // key code.
   void KeyPress(int key_code);
+
+  // Registers that a key is released by the player, given the current key code.
   void KeyRelease(int key_code);
+
+  // Draws all objects on the screen, including the background, the player, all
+  // debris, all bullets, and the platforms.
   void Draw();
+
+  // Starts the engine. If it is not started, Step() will not do anything.
   void Start();
+
+  // Ends the game, but the game engine will continue to run.
   void End();
+
+  // Checks whether or not the game engine is running.
   bool IsRunning();
+
+  // Checks whether or not the current game (within the engine) is over.
   bool IsOver();
+
+  // Tells the game engine where the mouse is currently located so the player
+  // can face that direction. This position is given in pixels.
   void UpdateMousePos(const cinder::ivec2& px_pos);
+
+  // Tells the engine that the player has clicked at this location, meaning the
+  // player should shoot in that direction. This location is given in pixels.
   void Shoot(int x_px, int y_px);
+
+  // Returns a reference to the player object.
   Player& GetPlayer();
+
+  // Spawns debris at the location, which is in pixels. Returns a pointer to
+  // this debris object.
   Debris* SpawnDebris(int x_px, int y_px);
+
+  // Spawns a bullet at the location, which is given in pixels. Returns a
+  // pointer to this bullet object.
   Bullet* SpawnBullet(int x_px, int y_px);
+
+  // Returns the number of seconds since Start() was last called.
   int GetSecondsSinceStart();
 
 private:
+
+  // The world object that houses all the b2Body's and all the physics in the
+  // game.
   b2World* world_;
+
+  // The Player object that the player controls.
   Player player_;
+
+  // The given screen width, in pixels.
   int screen_width_;
+
+  // The given screen height, in pixels.
   int screen_height_;
+
+  // The conversion factor from meters to pixels, given the current resolution.
   float32 m_to_px;
+
+  // The conversion factor from pixels to meters, given the current resolution.
   float32 px_to_m;
+
+  // Whether or not the game engine is running.
   bool running_ = false;
+
+  // Whether or not the game is over (the player is dead).
   bool over_ = false;
+
+  // A set of key codes that stores what keys are currently being held down.
+  // When a key is released, it is removed from this list.
   std::set<int> held_keys_;
+
+  // A vector storing pointers to all of the debris objects currently in the
+  // world.
   std::vector<Debris*> all_debris_;
+
+  // A vector storing pointers to all of the bullet objects currently in the
+  // world.
   std::vector<Bullet*> all_bullets_;
+
+  // A vector storing pointers to all of the platform bodies currently in the
+  // world.
   std::vector<b2Body*> all_platforms_;
+
+  // The time at which Start() was last called.
   std::chrono::time_point<std::chrono::system_clock> start_time_;
+
+  // The time at which SpawnDebris() was last called.
   std::chrono::time_point<std::chrono::system_clock> last_debris_time_;
+
+  // The number of debris the player shot this game.
   int num_debris_shot_ = 0;
+
+  // The number of debris that the player has has "near misses" with this game.
   int num_near_missed_ = 0;
+
+  // The current calculated score of the player.
   int score_ = 0;
+
+  // The current mouse position of the player.
   cinder::ivec2 mouse_pos_;
 
-  // Textures
+  // The background texture reference.
   cinder::gl::TextureRef background_texture_;
+
+  // The platform texture reference.
   cinder::gl::TextureRef platform_texture_;
 
+  // Tells the player object what keys are currently being pressed, and applies
+  // forces in the appropriate directions given the current keys being pressed.
   void UpdatePlayer();
+
+  // Created the boundaries of the map, including the outer walls and the
+  // platforms.
   void CreateBoundaries();
+
+  // Draws the hitboxes (the b2Body's) of all of the bodies in the world.
   void DrawHitBoxes();
+
+  // Draws the GUI of the game.
   void DrawGui();
+
+  // Draws the background texture.
   void DrawBackground();
+
+  // Draws the animation of all of the current debris bodies.
   void DrawDebris();
+
+  // Draws the animation of the player body.
   void DrawPlayer();
+
+  // Draws the animation of all of the current bullet bodies.
   void DrawBullets();
+
+  // Draws the platform textures.
   void DrawPlatforms();
+
+  // Checks all of the debris collisions, and deletes them if they are not
+  // colliding with other debris.
   void CheckDebrisCollisions();
+
+  // Checks all of the bullets to see if they are colliding with anything. If
+  // they are colliding with debris, it will increment the score. If they are
+  // colliding with a static body, then it will delete the bullet. If the bullet
+  // has been in the world for greater that kBulletLifetime seconds, then it
+  // deletes the bullet.
   void CheckBullets();
+
+  // Checks whether or not a debris should be spawned, given the current spawn
+  // interval.
   void CheckDebrisSpawn();
-  void CheckNearMisses();
-  void UpdateScore();
-  void LoadTextures();
+
+  // Gets the current interval at which debris should be spawning, determined
+  // by the equation: seconds = kIntercept + (time in seconds) / kSlope
   std::chrono::milliseconds GetDebrisSpawnInterval();
+
+  // Checks whether or not the player is close enough to any debris to count
+  // as a "near miss". If it does count as one, then the score is incremented
+  // and the debris is marked as already been near missed.
+  void CheckNearMisses();
+
+  // Calculates the score given the current number of near misses, the time, and
+  // the number of debris that have been shot.
+  void UpdateScore();
+
+  // Pre-loads all of the textures. This includes every texture but the player's
+  // (the player's texture is loaded when it is constructed).
+  void LoadTextures();
+
+  // Converts a pixel coordinate to a meter coordinate.
   b2Vec2 PxCoordsToMeterCoords(b2Vec2);
+
+  // Converts a meter coordinate to a pixel coordinate.
   b2Vec2 MeterCoordsToPxCoords(b2Vec2);
 
 };
